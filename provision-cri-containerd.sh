c071eb99655c07826794598216c1d9c877ccd7a7
Mitch Hulscher <mitch.hulscher@lib.io>
2018-01-28 14:06:08 +0100

    updated cri-containerd runtime

--- provision-cri-containerd.sh
+++ provision-cri-containerd.sh
@@ -2,7 +2,13 @@
 
 set -eufo pipefail
 
+CRI_CONTAINERD_VERSION=1.0.0-beta.1
+
 # Install cri-containerd as runtime
+
+## enable forwarding
+echo net.ipv4.conf.all.forwarding=1 > /etc/sysctl.d/90-forwarding.conf
+systemctl restart systemd-sysctl
 
 ## weave cni
 mkdir -p /etc/cni/net.d
@@ -21,9 +27,9 @@ Environment="KUBELET_SYSTEM_PODS_ARGS=--container-runtime=remote --container-run
 EOF
 
 ## install cri-containerd
-wget -q --https-only --timestamping https://github.com/kubernetes-incubator/cri-containerd/releases/download/v1.0.0-beta.0/cri-containerd-1.0.0-beta.0.linux-amd64.tar.gz
-tar xf cri-containerd-1.0.0-beta.0.linux-amd64.tar.gz -C /
-rm cri-containerd-1.0.0-beta.0.linux-amd64.tar.gz
+wget -q --https-only --timestamping https://github.com/containerd/cri-containerd/releases/download/v${CRI_CONTAINERD_VERSION}/cri-containerd-${CRI_CONTAINERD_VERSION}.linux-amd64.tar.gz
+tar xf cri-containerd-${CRI_CONTAINERD_VERSION}.linux-amd64.tar.gz -C /
+rm cri-containerd-${CRI_CONTAINERD_VERSION}.linux-amd64.tar.gz
 
 systemctl daemon-reload
 systemctl enable containerd cri-containerd
